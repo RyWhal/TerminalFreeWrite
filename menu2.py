920fdeec57f8286fc3f1eb6ebe9ffb689a699efc
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-23 16:33:08 -0500

    Update menu2.py

--- menu2.py
+++ menu2.py
@@ -9,7 +9,7 @@ class display_menu:
         self.font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 12)
         self.menu_options = ["New Type Wryte", "Continue Type Wryte", "Settings", "Manual"]
         self.selected_index = 0
-        self.epd.TurnOnDisplay_Fast()
+        #self.epd.TurnOnDisplay_Fast()
 
     def draw_menu(self):
         image = Image.new('1', (self.epd.width, self.epd.height), 255)
@@ -17,17 +17,21 @@ class display_menu:
         for i, option in enumerate(self.menu_options):
             prefix = "> " if i == self.selected_index else "  "
             draw.text((10, 10 + i * 30), prefix + option, font=self.font, fill=0)
-        self.epd.display_Fast(self.epd.getbuffer(image))
+        self.epd.display(self.epd.getbuffer(image))
 
     def navigate_menu(self):
         while True:
             self.draw_menu()
             if keyboard.is_pressed('up') or keyboard.is_pressed('w'):
+                print("Up or W pressed")
                 self.selected_index = max(self.selected_index - 1, 0)
             elif keyboard.is_pressed('down') or keyboard.is_pressed('s'):
+                print("down or S pressed")
                 self.selected_index = min(self.selected_index + 1, len(self.menu_options) - 1)
+                print("Enter")
             elif keyboard.is_pressed('enter'):
                 break
+            keyboard.sleep(0.1)
 
     def cleanup(self):
         self.epd.sleep()
