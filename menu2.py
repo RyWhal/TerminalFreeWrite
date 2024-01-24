a88bddc38acfeb793fa740fc6a1b0f64c804959f
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-23 22:29:49 -0500

    Update menu2.py

--- menu2.py
+++ menu2.py
@@ -19,8 +19,7 @@ class display_menu:
         self.draw = ImageDraw.Draw(self.image)
 
     def update_buffer(self):
-        self.epd.display_Fast(self.epd.getbuffer(self.image))
-        time.sleep(.05)
+        self.epd.display_Partial(self.epd.getbuffer(self.image))
 
     def draw_menu(self):
         # Create the image
