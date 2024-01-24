d0652db443b0cf5a3fb64cd4bb47309961d4a2a3
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-24 11:09:36 -0500

    trying someone elses code :(

--- menu2.py
+++ menu2.py
@@ -7,6 +7,7 @@ class display_menu:
     def __init__(self):
         self.epd = epd2in9_V2.EPD()
         self.epd.init()
+        self.epd.Clear()
         self.font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 11)
         self.menu_options = ["New Type Wrytes", "Continue Type Wryte", "Settings", "Manual"]
         self.selected_index = 0
@@ -17,21 +18,23 @@ class display_menu:
         self.image = self.image.rotate(270)
         self.draw = ImageDraw.Draw(self.image)
         
-    
+        
     def partial_update_buffer(self):
         #generate display buffer for display
+        self.draw.rectangle((0, 0, self.epd.height, self.epd.width), fill=255)
         partial_buffer = self.epd.getbuffer(self.image)
         self.epd.display_Partial(partial_buffer)
 
     def full_update_buffer(self):
         #generate display buffer for display
+        self.draw.rectangle((0, 0, self.epd.height, self.epd.width), fill=255)
         partial_buffer = self.epd.getbuffer(self.image)
         self.epd.display(partial_buffer)
     
     def draw_menu(self):
         #self.draw.text((10, 30), "TESTING", font=self.font, fill=0)
         self.draw.rectangle((0, 0, self.epd.width, self.epd.height), fill=255)
-        self.partial_update_buffer()
+        #self.partial_update_buffer()
 
         # Create the image
         # Clear the main display area -- also clears input line (270-300)
@@ -44,7 +47,8 @@ class display_menu:
         
         self.partial_update_buffer()
     
-    def navigate_menu(self):       
+    def navigate_menu(self):
+        self.full_update_buffer()       
         while True:
             self.draw_menu()
             if keyboard.is_pressed('up') or keyboard.is_pressed('w'):
