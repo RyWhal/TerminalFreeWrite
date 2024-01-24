d114ec0d274b0edfebd0ae15f6599c12b316cd88
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-23 22:43:55 -0500

    Update menu2.py

--- menu2.py
+++ menu2.py
@@ -7,7 +7,7 @@ class display_menu:
     def __init__(self):
         self.epd = epd2in9_V2.EPD()
         #self.epd.init()
-        self.epd.init_fast(self.epd.Seconds_1_5S)
+        self.epd.init_Fast()
         self.epd.Clear()
         self.font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 11)
         self.menu_options = ["New Type Wrytes", "Continue Type Wryte", "Settings", "Manual"]
