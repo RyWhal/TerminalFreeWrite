07552fbe21f9d030e8b7c609bf8108db6391781d
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-23 21:56:35 -0500

    Update menu2.py

--- menu2.py
+++ menu2.py
@@ -1,26 +1,27 @@
-from waveshare_epd import epd4in2_V3
+from waveshare_epd import epd4in2_V2
 from PIL import Image, ImageDraw, ImageFont
 import keyboard
 import time
 
 class display_menu:
     def __init__(self):
-        self.epd = epd4in2_V3.EPD()
-        self.epd.init()
-        #self.epd.init_fast(self.epd.Seconds_1_5S)
+        self.epd = epd4in2_V2.EPD()
+        #self.epd.init()
+        self.epd.init_fast(self.epd.Seconds_1_5S)
         self.epd.Clear()
         self.font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 11)
         self.menu_options = ["New Type Wrytes", "Continue Type Wryte", "Settings", "Manual"]
         self.selected_index = 0
-        #self.epd.TurnOnDisplay_Fast()
+        
 
         #Initialize display-related variables)
         self.image = Image.new('1', (self.epd.width, self.epd.height), 255)
         self.draw = ImageDraw.Draw(self.image)
 
     def update_buffer(self):
-        self.epd.display(self.epd.getbuffer(self.image))
-        time.sleep(.1)
+        self.epd.TurnOnDisplay_Partial()
+        self.epd.display_Partial(self.epd.getbuffer(self.image))
+        time.sleep(.05)
 
     def draw_menu(self):
         # Create the image
@@ -44,6 +45,7 @@ class display_menu:
                 self.selected_index = min(self.selected_index + 1, len(self.menu_options) - 1)
             elif keyboard.is_pressed('enter'):
                 print("Enter")
+                self.epd.sleep()
                 break
             time.sleep(.05)
 
