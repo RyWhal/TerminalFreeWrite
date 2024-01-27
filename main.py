0615bc704f5156bf7fc8f02419e929e48630a56b
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-27 11:25:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from text_display import text_display
+#from text_display import text_display
+from main_menu import main_menu
 
 def main():
-    app = text_display()
-    app.run_text_display()
+    #app = text_display()
+    #app.run_text_display()
+    app = main_menu()
+    app.run_main_menu()
 
 if __name__ == "__main__":
     main()
