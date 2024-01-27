0994131ae0de9aa649c66c8d7c96f6261790c4b1
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-27 12:12:51 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -80,7 +80,7 @@ class main_menu:
         #global current_selection
         if self.current_selection == 0:
             logging.info("trigger text display")
-            run_text_display()
+            text_display().run_text_display()
         elif self.current_selection == 1:
             # Trigger function for "Continue a freewrite"
             print ("option 2")
