c8220ac1b11ec50d81740f7906737e99a71c6c63
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-27 21:18:20 -0500

    adjusting ascii art

--- TypeWryter.py
+++ TypeWryter.py
@@ -140,10 +140,10 @@ class TypeWryter:
 
     def splash_screen(self):
         # Starting Y position
-        y_position = 10 
+        y_position = 30 
         # Add each line of the ASCII art to the image
         for line in self.ascii_art_lines:
-            self.display_draw.text((10, y_position), line, font=self.font13, fill=0)
+            self.display_draw.text((30, y_position), line, font=self.font13, fill=0)
             y_position += 12  # Adjust line spacing as needed
 
         # Update the display with the new image
